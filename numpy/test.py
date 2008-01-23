f3c4e8a3549fcff26e8b4edcdaefa099665563f1
thomas.heller <thomas.heller@c0dd1100-a219-0410-9f2b-9bf602d07565>
2008-01-23 21:07:05 +0000

    Implemented a mixed Python/C version as array_struct, this is slightly more than a factor of 2 faster.

--- numpy/test.py
+++ numpy/test.py
@@ -2,11 +2,13 @@ import unittest
 import numpy
 from ctypes import *
 import ctypes_array
+import array_struct
 
-class Test(unittest.TestCase):
+class Test_Python(unittest.TestCase):
+    mod = ctypes_array
     def test_np_to_ct(self):
         z = numpy.zeros(32)
-        c = ctypes_array.as_ctypes(z)
+        c = self.mod.as_ctypes(z)
 
         self.failUnless(type(c) is c_double * 32)
 
@@ -16,7 +18,7 @@ class Test(unittest.TestCase):
 
     def test_ct_to_np(self):
         c = (c_int * 3 * 2)(*((1, 2, 3), (4, 5, 6)))
-        n = ctypes_array.as_array(c)
+        n = self.mod.as_array(c)
 
         self.failUnless(n.base is c)
 
@@ -26,6 +28,9 @@ class Test(unittest.TestCase):
                 c[i][j] = 0
                 self.failUnlessEqual(c[i][j], n[i,j])
 
+class Test_C(Test_Python):
+    # same test, but uses the faster C version
+    mod = array_struct
         
         
 if __name__ == "__main__":
--- numpy/timeit.bat
+++ numpy/timeit.bat
@@ -1,4 +1,31 @@
-@rem 7.04 us
+@echo off
+
+rem
+rem ctypes_array is the pure Python implementation which uses the '__array_interface__' property.
+rem
+
+rem 7.04 us
+echo as_ctypes (__array_interface__)
 python -m timeit -s "import numpy; n = numpy.zeros(32); from ctypes_array import as_ctypes" "as_ctypes(n)" 
-@rem 7.91 us
-python -m timeit -s "from ctypes import c_int; from ctypes_array import as_array; n = (c_int * 3 * 2)()" "as_array(n)" 
+rem 7.91 us
+echo as_array (__array_interface__)
+python -m timeit -s "from ctypes import c_double; from ctypes_array import as_array; n = (c_double * 3 * 2)()" "as_array(n)" 
+
+REM How fast is the property access?
+REM 2.34 us
+REM python -m timeit -s "from ctypes import c_double; import ctypes_array; x = c_double()" "x.__array_interface__" 
+REM 0.45 us
+REM python -m timeit -s "from ctypes import c_double; import array_struct; x = c_double()" "x.__array_struct__" 
+
+
+rem
+rem array_struct is the mixed Python/C implementation which uses the '__array_struct__' property.
+rem faster by a factor of 2 (implementation time was more than double the time for the Python version)
+rem
+
+rem 3.23
+echo as_ctypes (__array_struct__)
+python -m timeit -s "import numpy; n = numpy.zeros(32); from array_struct import as_ctypes" "as_ctypes(n)" 
+rem 3.77
+echo as_array (__array_struct__)
+python -m timeit -s "from ctypes import c_double; from array_struct import as_array; n = (c_double * 32)()" "as_array(n)" 
