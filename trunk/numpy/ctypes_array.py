0b9b14f3b9ea4105543a844ad513b35b1424bf32
thomas.heller <thomas.heller@c0dd1100-a219-0410-9f2b-9bf602d07565>
2008-01-23 08:25:39 +0000

    Check incoming __array_interface__ descriptors for correct version, readonly flag, and strides.

--- trunk/numpy/ctypes_array.py
+++ trunk/numpy/ctypes_array.py
@@ -113,10 +113,17 @@ def as_ctypes(obj):
     """Create and return a ctypes object from a numpy array.  Actually
     anything that exposes the __array_interface__ is accepted."""
     ai = obj.__array_interface__
+    if ai["strides"]:
+        raise TypeError("strided arrays not supported")
+    if ai["version"] != 3:
+        raise TypeError("only __array_interface__ version 3 supported")
+    addr, readonly = ai["data"]
+    if readonly:
+        # XXX should make a copy in this case
+        raise TypeError("readonly arrays unsupported")
     tp = _typecodes[ai["typestr"]]
     for dim in ai["shape"][::-1]:
         tp = tp * dim
-    addr = ai["data"][0]
     result = tp.from_address(addr)
     result.__keep = ai
     return result
