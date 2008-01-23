c88b25d3f643dd2cf165f7512d2cab204a8c7a90
thomas.heller <thomas.heller@c0dd1100-a219-0410-9f2b-9bf602d07565>
2008-01-23 07:35:21 +0000

    Mow that 'as_array' doesn't make a copy it is very slightly faster.

--- numpy/timeit.bat
+++ numpy/timeit.bat
@@ -1,4 +1,4 @@
-@rem 6.84 us
+@rem 6.73 us
 python -m timeit -s "import numpy; n = numpy.zeros(32); from ctypes_array import as_ctypes" "as_ctypes(n)" 
 @rem 8.53 us
 python -m timeit -s "from ctypes import c_int; from ctypes_array import as_array; n = (c_int * 3 * 2)()" "as_array(n)" 
