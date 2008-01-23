6a7fb70bfd30867a786f2e4c8cd44092eefa126e
thomas.heller <thomas.heller@c0dd1100-a219-0410-9f2b-9bf602d07565>
2008-01-23 08:25:39 +0000

    Check incoming __array_interface__ descriptors for correct version, readonly flag, and strides.

--- numpy/timeit.bat
+++ numpy/timeit.bat
@@ -1,4 +1,4 @@
-@rem 6.73 us
+@rem 7.04 us
 python -m timeit -s "import numpy; n = numpy.zeros(32); from ctypes_array import as_ctypes" "as_ctypes(n)" 
 @rem 7.91 us
 python -m timeit -s "from ctypes import c_int; from ctypes_array import as_array; n = (c_int * 3 * 2)()" "as_array(n)" 
