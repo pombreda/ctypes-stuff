add839147f2c9eb0f75d5b23497367af8e1edf7c
thomas.heller <thomas.heller@c0dd1100-a219-0410-9f2b-9bf602d07565>
2009-01-12 08:51:46 +0000

    Pure virtual functions.

--- trunk/ctypes-cpp/mydll.h
+++ trunk/ctypes-cpp/mydll.h
@@ -1,4 +1,6 @@
-class __declspec(dllexport) CSimpleClass {
+#define EXPORT __declspec(dllexport)
+
+class EXPORT CSimpleClass {
   public:
 	int value;
 	CSimpleClass(int value);
@@ -7,6 +9,7 @@ class __declspec(dllexport) CSimpleClass {
 	void M1(int x);
 	virtual void V0();
 	virtual void V1(int x);
-	virtual void V2();
 	virtual void V1();
+	virtual void V1(char *ptr) = 0;
+	virtual void V2();
 };
