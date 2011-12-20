126ae710562fc5ee711275c8a15f10cd5377f262
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2011-12-20 14:20:31 +0000

    Some resource and version functions, make win64 compatible.

--- winsxs/wapi.py
+++ winsxs/wapi.py
@@ -5,6 +5,7 @@ from __future__ import division, with_statement, absolute_import
 from ctypes import *
 
 _kernel32 = WinDLL("kernel32")
+_version = WinDLL("version")
 
 def nonnull(result, func, args):
     if result:
@@ -15,6 +16,8 @@ BOOL_errcheck = nonnull
 HDC_errcheck = nonnull
 HENHMETAFILE_errcheck = nonnull
 HFONT_errcheck = nonnull
+
+_WIN64 = (sizeof(c_void_p) == 8)
 
 WSTRING = c_wchar_p
 class tagACTCTXW(Structure):
@@ -46,7 +49,10 @@ PCACTCTXW = POINTER(ACTCTXW)
 CreateActCtxW = _kernel32.CreateActCtxW
 CreateActCtxW.restype = HANDLE
 CreateActCtxW.argtypes = [PCACTCTXW]
-ULONG_PTR = c_ulong
+if _WIN64:
+    ULONG_PTR = c_ulonglong
+else:
+    ULONG_PTR = c_ulong
 BOOL = c_int
 ActivateActCtx = _kernel32.ActivateActCtx
 ActivateActCtx.restype = BOOL
@@ -80,3 +86,61 @@ def MAKELANGID(p,s): return (s << 10) | p
 LANG_ENGLISH = 9 # Variable c_int
 SUBLANG_ENGLISH_US = 1 # Variable c_int
 ACTCTX_FLAG_RESOURCE_NAME_VALID = 8
+GetModuleFileNameW = _kernel32.GetModuleFileNameW
+GetModuleFileNameW.restype = DWORD
+GetModuleFileNameW.argtypes = [HMODULE, LPWSTR, DWORD]
+GetModuleFileName = GetModuleFileNameW # alias
+STRING = c_char_p
+CHAR = c_char
+LPSTR = STRING
+GetModuleFileNameA = _kernel32.GetModuleFileNameA
+GetModuleFileNameA.restype = DWORD
+GetModuleFileNameA.argtypes = [HMODULE, LPSTR, DWORD]
+STRING = c_char_p
+HRSRC = HANDLE
+LPCSTR = STRING
+FindResourceA = _kernel32.FindResourceA
+FindResourceA.restype = HRSRC
+FindResourceA.argtypes = [HMODULE, LPCSTR, LPCSTR]
+HGLOBAL = HANDLE
+LoadResource = _kernel32.LoadResource
+LoadResource.restype = HGLOBAL
+LoadResource.argtypes = [HMODULE, HRSRC]
+LockResource = _kernel32.LockResource
+LockResource.restype = LPVOID
+LockResource.argtypes = [HGLOBAL]
+SizeofResource = _kernel32.SizeofResource
+SizeofResource.restype = DWORD
+SizeofResource.argtypes = [HMODULE, HRSRC]
+# GetFileVersionInfoSize = GetFileVersionInfoSizeW # alias
+LPDWORD = POINTER(DWORD)
+GetFileVersionInfoSizeA = _version.GetFileVersionInfoSizeA
+GetFileVersionInfoSizeA.restype = DWORD
+GetFileVersionInfoSizeA.argtypes = [LPCSTR, LPDWORD]
+GetFileVersionInfoA = _version.GetFileVersionInfoA
+GetFileVersionInfoA.restype = BOOL
+GetFileVersionInfoA.argtypes = [LPCSTR, DWORD, DWORD, LPVOID]
+GetFileVersionInfoA.errcheck = BOOL_errcheck
+class tagVS_FIXEDFILEINFO(Structure):
+    pass
+VS_FIXEDFILEINFO = tagVS_FIXEDFILEINFO
+tagVS_FIXEDFILEINFO._fields_ = [
+    ('dwSignature', DWORD),
+    ('dwStrucVersion', DWORD),
+    ('dwFileVersionMS', DWORD),
+    ('dwFileVersionLS', DWORD),
+    ('dwProductVersionMS', DWORD),
+    ('dwProductVersionLS', DWORD),
+    ('dwFileFlagsMask', DWORD),
+    ('dwFileFlags', DWORD),
+    ('dwFileOS', DWORD),
+    ('dwFileType', DWORD),
+    ('dwFileSubtype', DWORD),
+    ('dwFileDateMS', DWORD),
+    ('dwFileDateLS', DWORD),
+]
+PUINT = POINTER(c_uint)
+VerQueryValueA = _version.VerQueryValueA
+VerQueryValueA.restype = BOOL
+VerQueryValueA.argtypes = [LPVOID, LPSTR, POINTER(POINTER(VS_FIXEDFILEINFO)), PUINT]
+VerQueryValueA.errcheck = BOOL_errcheck
