69c456106bf0a5970d6810d1e37ff58a230c5b97
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2011-12-20 14:22:53 +0000

    Retrieve assembly from thy Python dll, parse, extend, and write out using elemnttree.

--- trunk/winsxs/create_assembly.py
+++ trunk/winsxs/create_assembly.py
@@ -1,67 +1,72 @@
 # -*- coding: latin-1 -*-
-"""Create a private sxs assembly containing a Python2.6 interpreter
+"""Create a private sxs assembly containing a Python interpreter
 """
 from __future__ import division, with_statement, absolute_import
 import os
 import shutil
 import wapi
+import xml.etree.ElementTree as ET
 
-manifest = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-    <noInheritable/>
-    <assemblyIdentity
-        type="win32"
-        name="python26.manifest"
-        version="2.6.6.0"
-        processorArchitecture="x86"
-    />
-    <file name="python26.dll" />
-    <file name="bz2.pyd" />
-    <file name="pyexpat.pyd" />
-    <file name="select.pyd" />
-    <file name="unicodedata.pyd" />
-    <file name="winsound.pyd" />
-    <file name="_bsddb.pyd" />
-    <file name="_ctypes.pyd" />
-    <file name="_ctypes_test.pyd" />
-    <file name="_elementtree.pyd" />
-    <file name="_hashlib.pyd" />
-    <file name="_msi.pyd" />
-    <file name="_multiprocessing.pyd" />
-    <file name="_socket.pyd" />
-    <file name="_sqlite3.pyd" />
-    <file name="_ssl.pyd" />
-    <file name="_testcapi.pyd" />
-    <file name="sqlite3.dll" />
-    <file name="tcl85.dll" />
-    <file name="tclpip85.dll" />
-    <file name="tk85.dll" />
-    <dependency>
-      <dependentAssembly>
-        <assemblyIdentity type="win32" name="Microsoft.VC90.CRT" version="9.0.21022.8" processorArchitecture="x86" publicKeyToken="1fc8b3b9a1e18e3b"></assemblyIdentity>
-      </dependentAssembly>
-    </dependency>
-</assembly>
-"""
+
+def GetModuleFileName(hmod):
+    buf = wapi.create_unicode_buffer(256)
+    wapi.GetModuleFileNameW(hmod, buf, 256)
+    return buf.value
 
 if __name__ == "__main__":
-    if os.path.exists("python26.private"):
-        print "Removing old python26.private directory..."
-        shutil.rmtree("python26.private")
+    import sys
+    dllpath = GetModuleFileName(sys.dllhandle)
 
-    print "Copying Python26 installation into python26.private..."
-    shutil.copytree("c:\\python26\\DLLs", "python26.private")
+    assembly = "python%d%d.private" % sys.version_info[:2]
 
-    print "Copying Python26.dll..."
-    shutil.copyfile("c:\\windows\\system32\\python26.dll", "python26.private\\python26.dll")
+    if os.path.exists(assembly):
+        print "Removing old %s directory..." % assembly
+        shutil.rmtree(assembly)
 
-    # Remove the RT_MANIFEST resource from the private copy of python26.dll
-    print "Replacing RT_MANIFEST in Python26.dll..."
-    h = wapi.BeginUpdateResource("python26.private\\python26.dll", False)
+    print "Copying Python installation into %s..." % assembly
+    import _socket; extpath = os.path.dirname(_socket.__file__)
+    shutil.copytree(extpath, assembly)
+
+    print "Copying", os.path.basename(dllpath)
+    shutil.copyfile(dllpath, os.path.join(assembly, os.path.basename(dllpath)))
+    files = os.listdir(assembly)
+
+    # Retrieve the manifest resource (FT_MANIFEST, id=2)
+    hr = wapi.FindResourceA(sys.dllhandle, "#2", wapi.LPCSTR(wapi.RT_MANIFEST))
+    hglobal = wapi.LoadResource(sys.dllhandle, hr)
+    size = wapi.SizeofResource(sys.dllhandle, hr)
+    ptr = wapi.LockResource(hglobal)
+    import ctypes
+    lpstr = ctypes.cast(ptr, ctypes.POINTER(ctypes.c_char))
+    m = lpstr[:size]
+
+    e = ET.fromstring(m)
+    for name in files:
+        e.insert(0, ET.Element("ns0:file", name=name))
+    manifest64 = ET.tostring(e)
+
+    # Retrieve the file version number
+    verinfosize = wapi.GetFileVersionInfoSizeA(dllpath, wapi.DWORD())
+    buf = wapi.create_string_buffer(verinfosize)
+    wapi.GetFileVersionInfoA(dllpath, 0, verinfosize, buf)
+
+    pfi = wapi.POINTER(wapi.VS_FIXEDFILEINFO)()
+
+    wapi.VerQueryValueA(buf, "\\", pfi, wapi.c_uint())
+    fi = pfi[0]
+    ver = divmod(fi.dwFileVersionMS, 0x10000) + divmod(fi.dwFileVersionLS, 0x10000)
+    print "Version %d.%d.%d.%d" % ver
+
+    # Remove the RT_MANIFEST resource from the private copy of pythonXY.dll
+    print "Replacing RT_MANIFEST in %s..." % os.path.basename(dllpath)
+    h = wapi.BeginUpdateResource(os.path.join(assembly, os.path.basename(dllpath)), False)
     wapi.UpdateResource(h,
                         wapi.MAKEINTRESOURCE(wapi.RT_MANIFEST),
                         wapi.MAKEINTRESOURCE(2),
                         0x0409,
-                        manifest,
-                        len(manifest))
+                        manifest64,
+                        len(manifest64))
     wapi.EndUpdateResource(h, False)
+
+    print
+    print manifest64
