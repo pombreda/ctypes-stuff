65072847d91425658ccc2e8b43196bd1dd93cc80
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2013-02-12 21:14:23 +0000

    *** empty log message ***

--- mf/test_mf.py
+++ mf/test_mf.py
@@ -39,7 +39,56 @@ class MFTest(unittest.TestCase):
                           "pep328.subpackage2",
                           "pep328.subpackage2.moduleZ",
                           })
-        self.assertEqual(mf.missing(), [])
+        self.assertFalse(mf.missing())
+
+
+    # /python33/lib/site-packages/nose/tools/nontrivial.py
+
+
+    def test_tools(self):
+        """
+        check that these are not reported as missing modules:
+        
+        - testmods.tools.bar
+        - testmods.tools.baz
+        - testmods.tools.spam
+        - testmods.tools.foo
+        """
+        
+        src = """
+        The package structure is like this:
+        
+        testmods/test_tools.py:
+            import testmods.tools.bar
+            import testmods.tools.baz
+            import testmods.tools.spam
+            import testmods.tools.foo
+
+        testmods/tools/spamfoo.py:
+            spam = "spam"
+            bar = "bar"
+
+        testmods/tools/bazbar.py
+            baz = "baz"
+            bar = "bar"
+
+        testmods/tools/__init__.py
+            from .bazbar import *
+
+        testmods/tools/__init__.py
+            from .spamfoo import spam, foo
+
+        """
+
+        mf = ModuleFinder(debug=debug)
+        mf.import_hook("testmods.test_tools")
+        self.assertEqual(set(mf.modules.keys()), {
+            "testmods",
+            "testmods.test_tools",
+            "testmods.tools",
+            "testmods.tools.bazbar",
+            "testmods.tools.spamfoo"})
+        self.assertFalse(mf.missing())
 
 if __name__ == "__main__":
     unittest.main()
