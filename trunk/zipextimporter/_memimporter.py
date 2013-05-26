e1682f761ee1a60aef4ea0e31cc70910c60a20a9
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2013-05-26 16:28:07 +0000

    Porting py2exe's zipextimporter to the current version of Joachim Bauchs MemoryModule.

---
+++ trunk/zipextimporter/_memimporter.py
@@ -0,0 +1,14 @@
+# This help module allows to import the _memimporter.pyd extension
+# which is in the distutils build directory.
+
+def __load():
+    import imp, os, sys, struct
+    if struct.calcsize("P") == 8:
+        dirname = "build\\lib.win-amd64-2.7"
+    else:
+        dirname = "build\\lib.win32-2.7"
+    path = os.path.abspath(os.path.join(dirname, '_memimporter.pyd'))
+    imp.load_dynamic("_memimporter", path)
+
+__load()
+del __load
---
+++ trunk/zipextimporter/source/_memimporter.c
@@ -0,0 +1,122 @@
+/*
+  For the _memimporter compiled into py2exe exe-stubs we need "Python-dynload.h".
+  For the standalone .pyd we need <Python.h>
+*/
+
+#include <Python.h>
+#include <windows.h>
+
+static char module_doc[] =
+"Importer which can load extension modules from memory";
+
+#include "MemoryModule.h"
+#include "actctx.h"
+
+static FARPROC _GetProcAddress(HCUSTOMMODULE module, LPCSTR name, void *userdata)
+{
+	FARPROC res;
+	res = (FARPROC)GetProcAddress((HMODULE)module, name);
+	if (res == NULL) {
+		SetLastError(0);
+		return MemoryGetProcAddress(module, name);
+	} else
+		return res;
+}
+
+static void _FreeLibrary(HCUSTOMMODULE module, void *userdata)
+{
+    FreeLibrary((HMODULE) module);
+}
+
+static HCUSTOMMODULE _LoadLibrary(LPCSTR filename, void *userdata)
+{
+	HCUSTOMMODULE result;
+//	printf("LoadLibrary(%s)\n", filename);
+	if (userdata) {
+		PyObject *findproc = (PyObject *)userdata;
+		PyObject *res = PyObject_CallFunction(findproc, "s", filename);
+		if (res && PyString_AsString(res)) {
+			result = MemoryLoadLibraryEx(PyString_AsString(res),
+						     _LoadLibrary, _GetProcAddress, _FreeLibrary,
+						     userdata);
+			Py_DECREF(res);
+			printf("MemoryLoadLibrary(%s) -> %p\n", filename, (void *)result);
+			return (HCUSTOMMODULE)result;
+		} else {
+			PyErr_Clear();
+		}
+	}
+	return (HCUSTOMMODULE)LoadLibraryA(filename);
+}
+
+static PyObject *
+import_module(PyObject *self, PyObject *args)
+{
+	char *data;
+	int size;
+	char *initfuncname;
+	char *modname;
+	char *pathname;
+	HMEMORYMODULE hmem;
+	FARPROC do_init;
+
+	char *oldcontext;
+	ULONG_PTR cookie = 0;
+	PyObject *findproc;
+	/* code, initfuncname, fqmodulename, path */
+	if (!PyArg_ParseTuple(args, "s#sss|O:import_module",
+			      &data, &size,
+			      &initfuncname, &modname, &pathname,
+			      &findproc))
+		return NULL;
+    
+	cookie = _My_ActivateActCtx();//try some windows manifest magic...
+
+	hmem = MemoryLoadLibraryEx(data, _LoadLibrary, _GetProcAddress, _FreeLibrary, (void *)findproc);
+	printf("MemoryLoadLibrary(%s) -> %p\n", pathname, (void *)hmem);
+
+	_My_DeactivateActCtx(cookie);
+	if (!hmem) {
+		printf("ERROR %d\n", GetLastError());
+		PyErr_Format(PyExc_ImportError,
+			     "MemoryLoadLibrary failed loading %s", pathname);
+		return NULL;
+	}
+	do_init = MemoryGetProcAddress(hmem, initfuncname);
+	if (!do_init) {
+		MemoryFreeLibrary(hmem);
+		PyErr_Format(PyExc_ImportError,
+			     "Could not find function %s", initfuncname);
+		return NULL;
+	}
+
+        oldcontext = _Py_PackageContext;
+	_Py_PackageContext = modname;
+	do_init();
+	_Py_PackageContext = oldcontext;
+	if (PyErr_Occurred())
+		return NULL;
+	/* Retrieve from sys.modules */
+	return PyImport_ImportModule(modname);
+}
+
+static PyObject *
+get_verbose_flag(PyObject *self, PyObject *args)
+{
+	return PyInt_FromLong(Py_VerboseFlag);
+}
+
+static PyMethodDef methods[] = {
+	{ "import_module", import_module, METH_VARARGS,
+	  "import_module(code, initfunc, modname, pathname[, finder]) -> module" },
+	{ "get_verbose_flag", get_verbose_flag, METH_NOARGS,
+	  "Return the Py_Verbose flag" },
+//	{ "set_find_proc", set_find_proc, METH_VARARGS },
+	{ NULL, NULL },		/* Sentinel */
+};
+
+DL_EXPORT(void)
+init_memimporter(void)
+{
+	Py_InitModule3("_memimporter", methods, module_doc);
+}
