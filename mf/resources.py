98f89b6a7b4d90a97d0c8a73814a5ddb8bb03275
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2013-08-02 19:59:44 +0000

    Pack python.dll

--- mf/resources.py
+++ mf/resources.py
@@ -5,20 +5,36 @@
 
 import _wapi
 
+# something like this is what we want
+## import contextlib
+
+## @contextlib.contextmanager
+## def UpdateResources(filename, delete_existing=False):
+##     hrscr = _wapi.BeginUpdateResourceW(filename, delete_existing)
+##     yield _wapi.UpdateResourceA
+##     _wapi.EndUpdateResourceW(hrsrc, False)
+    
+
+## def add_resources(filename, script_info):
+##     with UpdateResources(filename) as add:
+##         add(b"PYTHON33.DLL", 1, pydll_bytes)
+##         add(b"PYTHONSCRIPT", 1, script_info)
+
+
 def add_resources(filename, script_info):
     hrsrc = _wapi.BeginUpdateResourceW(filename, False)
 
     print("Adding Resources")
 
-    ## with open("c:\\windows\\system32\\python33.dll", "rb") as ifi:
-    ##     pydll_bytes = ifi.read()
+    with open("c:\\windows\\system32\\python33.dll", "rb") as ifi:
+        pydll_bytes = ifi.read()
 
-    ## _wapi.UpdateResourceA(hrsrc,
-    ##                       b"PYTHON33.DLL",
-    ##                       _wapi.LPCSTR(1),
-    ##                       0, # wLanguage
-    ##                       pydll_bytes,
-    ##                       len(pydll_bytes));
+    _wapi.UpdateResourceA(hrsrc,
+                          b"PYTHON33.DLL",
+                          _wapi.LPCSTR(1),
+                          0, # wLanguage
+                          pydll_bytes,
+                          len(pydll_bytes));
 
     _wapi.UpdateResourceA(hrsrc,
                           b"PYTHONSCRIPT",
