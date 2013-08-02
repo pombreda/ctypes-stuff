4a884a4423baef4d2fa1382a8ae185be6987d1b5
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2013-08-02 19:20:18 +0000

    WIP.

--- mf/resources.py
+++ mf/resources.py
@@ -5,27 +5,26 @@
 
 import _wapi
 
-def add_resources(filename):
+def add_resources(filename, script_info):
     hrsrc = _wapi.BeginUpdateResourceW(filename, False)
+
     print("Adding Resources")
-    ## _wapi.UpdateResourceW(hrsrc,
-    ##                       "PYTHONSCRIPT", # lpType
-    ##                       _wapi.LPCWSTR(1), # lpName
-    ##                       0, # wLanguage
-    ##                       "foo", # lpData
-    ##                       6)# cbData
+
+    ## with open("c:\\windows\\system32\\python33.dll", "rb") as ifi:
+    ##     pydll_bytes = ifi.read()
+
     ## _wapi.UpdateResourceA(hrsrc,
-    ##                       "PYTHONSCRIPT".encode("mbcs"), # lpType
-    ##                       _wapi.LPCSTR(2), # lpName
+    ##                       b"PYTHON33.DLL",
+    ##                       _wapi.LPCSTR(1),
     ##                       0, # wLanguage
-    ##                       "foo".encode("mbcs"), # lpData
-    ##                       3)# cbData
-    with open("c:\\windows\\system32\\python33.dll", "rb") as ifi:
-        pydll_bytes = ifi.read()
+    ##                       pydll_bytes,
+    ##                       len(pydll_bytes));
+
     _wapi.UpdateResourceA(hrsrc,
-                          b"PYTHON33.DLL",
+                          b"PYTHONSCRIPT",
                           _wapi.LPCSTR(1),
-                          0,
-                          pydll_bytes,
-                          len(pydll_bytes));
+                          0, # wLanguage
+                          script_info,
+                          len(script_info))
+
     _wapi.EndUpdateResourceW(hrsrc, False)
