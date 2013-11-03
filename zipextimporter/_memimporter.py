62e261101152a82d1d93484079ac141d2953201b
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2013-11-03 18:26:21 +0000

    Don't hardcode Python version.

--- zipextimporter/_memimporter.py
+++ zipextimporter/_memimporter.py
@@ -4,9 +4,9 @@
 def __load():
     import imp, os, sys, struct
     if struct.calcsize("P") == 8:
-        dirname = "build\\lib.win-amd64-2.7"
+        dirname = "build\\lib.win-amd64-%d.%d" % sys.version_info[:2]
     else:
-        dirname = "build\\lib.win32-2.7"
+        dirname = "build\\lib.win32-%d.%d" % sys.version_info[:2]
     path = os.path.abspath(os.path.join(dirname, '_memimporter.pyd'))
     imp.load_dynamic("_memimporter", path)
 
