8dc323b9b1b42be612a5bfb6b7f0534424a0fd61
thomas.heller@gmail.com <thomas.heller@gmail.com@c0dd1100-a219-0410-9f2b-9bf602d07565>
2013-11-05 19:24:38 +0000

    Works now with Python 2.6, 2.7, 3.3, and 3.4. 3.0, 3.1, and 3.2 are not supported.

--- trunk/zipextimporter/_memimporter.py
+++ trunk/zipextimporter/_memimporter.py
@@ -3,11 +3,17 @@
 
 def __load():
     import imp, os, sys, struct
-    if struct.calcsize("P") == 8:
-        dirname = "build\\lib.win-amd64-%d.%d" % sys.version_info[:2]
+    if hasattr(sys, "gettotalrefcount"):
+        suffix = "-pydebug"
+        pyd = "_d.pyd"
     else:
-        dirname = "build\\lib.win32-%d.%d" % sys.version_info[:2]
-    path = os.path.abspath(os.path.join(dirname, '_memimporter.pyd'))
+        suffix = ""
+        pyd = ".pyd"
+    if struct.calcsize("P") == 8:
+        dirname = "build\\lib.win-amd64-%d.%d%s" % (sys.version_info[0], sys.version_info[1], suffix)
+    else:
+        dirname = "build\\lib.win32-%d.%d%s" % (sys.version_info[0], sys.version_info[1], suffix)
+    path = os.path.abspath(os.path.join(dirname, '_memimporter' + pyd))
     imp.load_dynamic("_memimporter", path)
 
 __load()
--- trunk/zipextimporter/setup.py
+++ trunk/zipextimporter/setup.py
@@ -1,15 +1,27 @@
-#import sys, os, string
 from distutils.core import setup, Extension
+
+extra_link_args = []
+extra_compile_args = []
+
+if 0:
+    # enable this to include debug info into a release build
+    extra_compile_args.append("/Z7")
+    extra_link_args.append("/DEBUG")
 
 _memimporter = Extension("_memimporter",
                          ["source/MemoryModule.c",
                           "source/MyLoadLibrary.c",
                           "source/_memimporter.c",
                           "source/actctx.c"],
-                         define_macros=[("STANDALONE", "1")],
+                         define_macros=[("STANDALONE", "1"),
+##                                        ("VERBOSE", "1"),
+                                        ("_CRT_SECURE_NO_WARNINGS", "1")],
+                         extra_compile_args=extra_compile_args,
+                         extra_link_args=extra_link_args,
                          )
+
 setup(name="zipextimporter",
-      description="import extension modules from zipfiles without unpacking them",
+      description="import extension modules from zipfiles without unpacking them to disk",
       version="0.2",
       author="Thomas Heller",
       author_email="theller@ctypes.org",
